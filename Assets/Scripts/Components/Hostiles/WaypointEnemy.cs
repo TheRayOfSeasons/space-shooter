d3d48f326a999a9c264165e214e0bc0a8bbc6145
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-08-22 21:58:26 +0800

    Feature: Waypoint Enemy

---
+++ Assets/Scripts/Components/Hostiles/WaypointEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpaceShooterEngine;
+
+public class WaypointEnemy : Enemy
+{
+    public List<Transform> destinations;
+    private Waypoint2D waypoints;
+
+    void Start()
+    {
+        List<Vector2> destinationVectors = new List<Vector2>();
+        foreach(Transform destination in destinations)
+            destinationVectors.Add(destination.position);
+        waypoints = new Waypoint2D(this, destinationVectors, 0.1f);
+    }
+
+    void Update()
+    {
+        waypoints.TravelToNextDestination(Time.deltaTime);
+    }
+}
