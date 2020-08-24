79e0555dc4dbbe9af8464cc4320827a083778b42
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-08-24 22:02:03 +0800

    Feature: Initial Spawner Script

---
+++ Assets/Scripts/Components/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SpaceShooterEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject toSpawn;
+    private TimedAction spawn;
+
+    void Start()
+    {
+        spawn = new TimedAction(5f, () => {
+            GameObject entity = Instantiate(toSpawn);
+            entity.transform.position = transform.position;
+        });
+    }
+
+    void Update()
+    {
+        spawn.Run(Time.deltaTime);
+    }
+}
