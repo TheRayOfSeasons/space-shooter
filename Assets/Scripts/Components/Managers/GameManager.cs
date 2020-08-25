ba8024634310cab5d78ed3684fb76325e066bfc6
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-08-25 20:34:36 +0800

    Feature: GameManager

---
+++ Assets/Scripts/Components/Managers/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private float score = 0f;
+    public float Score
+    {
+        get { return score; }
+    }
+
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void AddScore(float augment)
+    {
+        score += augment;
+    }
+
+    public void ResetScore()
+    {
+        score = 0f;
+    }
+}
