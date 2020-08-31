52b974d9ebc21490d8aaad5ad80842ede45b4321
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-08-31 22:33:26 +0800

    Add: UIManager

---
+++ Assets/Scripts/Components/Managers/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text score;
+
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        score.text = $"Score: {GameManager.Instance.Score}";
+    }
+}
