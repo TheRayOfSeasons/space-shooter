44f8b2ea5344d1a7366d81650fba20fd8ea3025d
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-10-04 17:11:25 +0800

    Add: Player Networking Logic

---
+++ Assets/Scripts/Core/PlayerNetworking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetworking : MonoBehaviour
+{
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private MonoBehaviour[] scriptsToIgnore;
+
+    public PhotonView photonView;
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        if(!photonView.isMine)
+        {
+            playerCamera.SetActive(false);
+            foreach(MonoBehaviour script in scriptsToIgnore)
+                script.enabled = false;
+        }
+    }
+}
