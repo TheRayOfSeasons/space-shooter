63d8dd4cb06cfaff23fc75d6c2e6528d73802f9a
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-10-17 11:24:51 +0800

    Add: Constants with helpers

---
+++ Assets/Scripts/Core/Constants.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Constants
+{
+    public class EntityColor
+    {
+        public static readonly string blue = "";
+        public static readonly string green = "";
+        public static readonly string red = "";
+        public static readonly string yellow = "";
+
+        public static List<string> GetAllColors()
+        {
+            return new List<string>(){
+                blue,
+                green,
+                red,
+                yellow
+            };
+        }
+
+        private static string GetRandomColorHexFromList(List<string> colors)
+        {
+            int index = Random.Range(0, colors.Count);
+            return colors[index];
+        }
+
+        public static string GetRandomColorHex()
+        {
+            List<string> colors = GetAllColors();
+            return GetRandomColorHexFromList(colors);
+        }
+
+        public static string GetRandomColorHex(List<string> excludes)
+        {
+            List<string> colors = GetAllColors();
+            colors.RemoveAll(color => excludes.Contains(color));
+            return GetRandomColorHexFromList(colors);
+        }
+    }
+}
