02fd79db87741bb376c9fd375ec39985661ca925
TheRayofSeasons <raylawrencehenrisison@gmail.com>
2020-10-18 13:24:46 +0800

    Add: Custom consistent color for obstacles

--- Assets/Scripts/Components/Obstacle.cs
+++ Assets/Scripts/Components/Obstacle.cs
@@ -5,6 +5,13 @@ using SpaceShooterEngine;
 
 public class Obstacle : Entity
 {
+    [SerializeField] private string color;
+
+    void Start()
+    {
+        this.AssignColor(Constants.EntityColor.hexCode[this.color]);
+    }
+
     public override void OnZeroHitPoints()
     {
         Destroy(gameObject);
